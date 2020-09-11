e902fe745893f2d07f33fdde52c153d9359f7cb3
Mahin24 <mahin242424@gmail.com>
2020-09-11 13:28:42 +0600

    imported uuid changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
-import uuid from 'uuid';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import {v4 as uuid} from 'uuid'
+import 'bootstrap/dist/css/bootstrap.min.css';
 //My First project and also i setup github
-function App() {
-  return (
-    <div className="App">
-       <TodoInput />
-       <TodoList />
-    </div>
-  );
+class App extends Component {
+  state ={
+    items : [{id:1,title:'wake up'},{id:2,title:'Hello Baby'}],
+    id : uuid(),
+    item:''
+  };
+
+  render() {
+    
+    return(
+      <div>
+        <div className="container">
+          <div className="row">
+            <TodoInput />
+            <TodoList />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
