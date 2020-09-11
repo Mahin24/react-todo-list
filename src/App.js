27b09ff257120c1116bdee9752ba3d72cbf976cc
Mahin24 <mahin242424@gmail.com>
2020-09-11 15:01:06 +0600

    App component structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React, { Component } from 'react';
-import TodoInput from './components/TodoInput';
-import TodoList from './components/TodoList';
-import {v4 as uuid} from 'uuid'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import { v4 as uuid } from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
 //My First project and also i setup github
 class App extends Component {
-  state ={
-    items : [{id:1,title:'wake up'},{id:2,title:'Hello Baby'}],
-    id : uuid(),
-    item:''
+  state = {
+    items: [
+      { id: 1, title: "wake up" },
+      { id: 2, title: "Hello Baby" },
+    ],
+    id: uuid(),
+    item: "",
+    editItem: false,
+  };
+
+  handleChange = (e) => {
+    console.log("handle change");
+  };
+  handleSubmit = (e) => {
+    console.log("handle submit");
+  };
+  clearList = () => {
+    console.log("clear list");
+  };
+  handleDelete = (id) => {
+    console.log(`handle delete ${id}`);
+  };
+  handleEdit = (id) => {
+    console.log(`handle edit ${id}`);
   };
 
   render() {
-    
-    return(
-      <div>
-        <div className="container">
-          <div className="row">
-            <TodoInput />
-            <TodoList />
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">TodoInput</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
           </div>
         </div>
       </div>
