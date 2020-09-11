f477caa3c5ed863ce9656722f2dc995e540dbc4f
Mahin24 <mahin242424@gmail.com>
2020-09-11 11:55:16 +0600

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <section>
+                <h2>This is from todo item</h2>
+            </section>
+        )
+    }
+}
