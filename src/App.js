65d6bb106037a43cf607ee0b3eb945264966d537
Mahin24 <mahin242424@gmail.com>
2020-09-11 12:09:12 +0600

    setup vs code in github

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 import uuid from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+//My First project and also i setup github
 function App() {
   return (
     <div className="App">
